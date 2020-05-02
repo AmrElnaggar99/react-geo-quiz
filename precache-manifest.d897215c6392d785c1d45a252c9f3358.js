53caff537a5872885af2abbf33bd8b91e59c531b
Amr Elnaggar <amr.eln2gar@gmail.com>
2020-05-02 05:35:01 +0200

    Updates

---
+++ precache-manifest.d897215c6392d785c1d45a252c9f3358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebeb27b730d4a8181dd866d6018ae0f5",
+    "url": "/react-geo-quiz/index.html"
+  },
+  {
+    "revision": "1c2701eb1270338ef414",
+    "url": "/react-geo-quiz/static/css/main.bdb9da3d.chunk.css"
+  },
+  {
+    "revision": "2d595a28ed0504260281",
+    "url": "/react-geo-quiz/static/js/2.b017ca32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-geo-quiz/static/js/2.b017ca32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c2701eb1270338ef414",
+    "url": "/react-geo-quiz/static/js/main.615ba8ec.chunk.js"
+  },
+  {
+    "revision": "22cc0f7a5af4f9d8fa47",
+    "url": "/react-geo-quiz/static/js/runtime-main.144310f8.js"
+  }
+]);
